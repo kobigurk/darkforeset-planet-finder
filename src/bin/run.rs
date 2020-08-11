26ddb8cbbf554c8bacb5ab6a796466d67f28c1c2
Kobi Gurkan <kobigurk@gmail.com>
2020-08-11 23:40:47 +0300

    enlarge the board

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -13,21 +13,36 @@ use rayon::prelude::*;
 fn main() {
     let mimc = Mimc7::new();
     let ub = BigInt::from_str("163080117641681993173408551106283628110202881696939724264280529220222").unwrap();
-    (0..128u64).into_par_iter().for_each(|yy| {
-        for y in (yy*64)..((yy+1)*64)  {
-            for xx in 0..8192 {
-                for i in 0..2 {
-                    if xx % 100 == 0 {
-                        println!("trying {}, {}", xx, y);
-                    }
-                    let x = if i == 0 {
-                        BigInt::new(Sign::Plus, vec![xx])
-                    } else if xx == 0 {
+    (0..128u64).into_par_iter().for_each(|yyy| {
+        let yyy = yyy as u32;
+        for yy in (yyy*282)..((yyy+1)*282)  {
+            for xx in 0..36000 {
+                if xx % 1000 == 0 {
+                    println!("trying {}, {}", xx, yy);
+                }
+
+                for i in 0..4 {
+
+                    let x = if xx == 0 {
                         BigInt::new(Sign::Plus, vec![0])
                     } else {
-                        BigInt::from_str("21888242871839275222246405745257275088548364400416034343698204186575808495617").unwrap() - BigInt::new(Sign::Plus, vec![xx])
+                        if i == 0 || i == 2 {
+                            BigInt::new(Sign::Plus, vec![xx])
+                        } else {
+                            BigInt::from_str("21888242871839275222246405745257275088548364400416034343698204186575808495617").unwrap() - BigInt::new(Sign::Plus, vec![xx])
+                        }
                     };
-                    let h = mimc.hash(vec![x.clone(), BigInt::new(Sign::Plus, vec![y as u32])]).unwrap();
+                    let y = if yy == 0 {
+                        BigInt::new(Sign::Plus, vec![0])
+                    } else {
+                        if i == 0 || i == 1 {
+                            BigInt::new(Sign::Plus, vec![yy])
+                        } else {
+                            BigInt::from_str("21888242871839275222246405745257275088548364400416034343698204186575808495617").unwrap() - BigInt::new(Sign::Plus, vec![yy])
+                        }
+                    };
+
+                    let h = mimc.hash(vec![x.clone(), y.clone()]).unwrap();
                     if h < ub {
                         let (_, bytes) = h.to_bytes_be();
                         let mut v = vec![0 as u8; 32];
@@ -64,7 +79,11 @@ fn main() {
                         } else {
                             "wtf"
                         };
-                        println!("found {}, {}, {}, {}", x, y, h, k);
+                        if i == 0 {
+                            println!("found {}, {}, {}, {}", x, y, h, k);
+                        } else {
+                            println!("found -{}, {}, {}, {}", x, y, h, k);
+                        }
                     }
             }
             }
